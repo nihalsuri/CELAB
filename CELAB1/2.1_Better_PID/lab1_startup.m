54f478a9c064f0bdff85e949f16e210e7876a6c6
6maxwell9 <pries.maximilian@t-online.de>
2025-04-01 15:24:00 +0200

    added a sctipt to plot and save data

--- CELAB1/2.1_Better_PID/lab1_startup.m
+++ CELAB1/2.1_Better_PID/lab1_startup.m
@@ -20,9 +20,9 @@ mld.tausf = 0.0056;     % [Nm]
 mld.Jeq = mld.Jeq;      % [kg m^2]
 
 % Actual Parameters (estimated from Motor 1)
-%mld.Beq = 2.5663e-6;    % [Nm/(rad/s)]
-%mld.tausf = 0.013;      % [Nm]
-%mld.Jeq = 3.4640e-07;   % [kg m^2]
+mld.Beq = 2.5663e-6;    % [Nm/(rad/s)]
+mld.tausf = 0.013;      % [Nm]
+mld.Jeq = 3.4640e-07;   % [kg m^2]
 
 
 % Validation Anti-Windup
@@ -38,7 +38,7 @@ feedforward.acc = 900;
 % time for which acceleration is applied [s]
 feedforward.time = 0.5;
 % number of cycles (pos. -> 0 -> neg. -> neg. -> 0 -> pos.)
-feedforward.num = 3;
+feedforward.num = 1;
 
 
 % Choice of Input => "Anti-Windup"->1 or "Feedforward"->2
---
+++ CELAB1/plot_for_report.m
@@ -0,0 +1,37 @@
+% Script to automate nice plotting from simulink
+clear simout legends xlab f1 save_name
+
+
+% Replace with the name of the output to plot and the filename for the .png
+simout = betterpid_omega_m;
+save_name = "windup_sim";
+% =========================================================================
+
+
+
+
+% Get the x-label from the name of the scope
+xlab = split(simout.blockName,"/");
+xlab = xlab(end);
+
+% Initialize cellarray
+legends = cell(length(simout.signals),1);
+
+% Plot all signals connected to the scope
+% Legend entries will be the names of the signal, same as in simulink
+f1 = figure(1);
+hold on
+for i=1:length(simout.signals)
+    plot(simout.time, simout.signals(i).values, "LineWidth", 1.5)
+    legends{i} = simout.signals(i).label;
+end
+grid on
+legend(legends{:})
+xlabel("Time [s]")
+ylabel(xlab)
+% a nice dimension to put into a report
+set(f1,'Position',[300 300 800 400])
+
+% save figure as a .png file and save the corresponding data
+saveas(f1, 'Data_and_Plots/'+save_name+'.png')
+save('Data_and_Plots/'+save_name+'.mat', "simout")
