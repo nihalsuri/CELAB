6fdfd94c740f4b2060366b374a326337cb82d822
nihalsuri <nihalsuri1619@gmail.com>
2025-05-11 00:02:04 +0200

    added completed resonant load model with dummy PID to compare with blackbox

--- CELAB3/21_SelfmadeResonantModel/selfmade_resonant_model_startup.m
+++ CELAB3/21_SelfmadeResonantModel/selfmade_resonant_model_startup.m
@@ -3,8 +3,6 @@
 % space controller with chosen eigenvalues.
 clear
 
-
-
 %% Load Predefined Parameters
 load_params_resonant_case
 
@@ -28,10 +26,24 @@ mld.K = 0.83;          % [Nm/rad]
 sIn.solver_time = 1e-4;
 
 % List of reference positions [s]
-sIn.position = [40, 0]; 
+sIn.position = [0, 50]; 
 
 % Time the reference positions are held [s]
-sIn.step_time = 5; 
+sIn.step_time = 2; 
 
 % Automatic calculation of total simulation time [s]
 sIn.simulation_time = sIn.step_time*length(sIn.position);
+
+%% PID Controller to test simulink model against blackbox 
+
+%PID = computePIDGains(8, 0.15, 0.1, plant.Ps, "PID");
+PID.Kp = 5.845;
+PID.Kd = 0.3;
+PID.Ki = 0.2;
+PID.Tl = 9.7252e-04;
+
+% Anit Windup
+PID.t_s5 = 0.85; % 5% settling time 
+PID.Kw = 1/(PID.t_s5/5); % anit windup gain: 1/Tw, Tw=t_s5/5
+
+
