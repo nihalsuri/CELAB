b5dcf283563662c90010833725b366845b043d0b
6maxwell9 <pries.maximilian@t-online.de>
2025-05-11 13:51:12 +0200

    udated sseval

--- CELAB3/23_StateSpace_Eigenvalues/lab3_startup_ssEvals.m
+++ CELAB3/23_StateSpace_Eigenvalues/lab3_startup_ssEvals.m
@@ -30,20 +30,24 @@ specs.settling_time = 0.85; % [s]
 sIn.solver_time = 1e-4;
 
 % List of reference positions [s]
-sIn.position = [40, 0, 70, 0, 120]; 
+sIn.position = 50; 
 
 % Time the reference positions are held [s]
-sIn.step_time = 5; 
+sIn.step_time = 10; 
 
 % Automatic calculation of total simulation time [s]
 sIn.simulation_time = sIn.step_time*length(sIn.position);
+
+% Time for averaging the bias of displacement sensor
+sIn.t0 = 0.2;
+sIn.t1 = 0.7;
 
 
 %% State-Space Model
 % with the state x=[theta_h, theta_d, omega_h, omega_d]
 
-tmp.v1 = 1 /gbox.N^2 /mld.Jeq;
-tmp.v2 = (mld.Beq + mot.Kt*mot.Ke) /mld.Jeq;
+tmp.v1 = 1 /(gbox.N^2 * mld.Jeq);
+tmp.v2 = (mld.Beq + mot.Kt*mot.Ke/mot.Req) /mld.Jeq;
 tmp.v3 = mot.Kt*drv.dcgain /gbox.N /mld.Jeq / mot.Req;
 
 plant.A = [zeros(2,2),    eye(2,2);
@@ -52,31 +56,31 @@ plant.A = [zeros(2,2),    eye(2,2);
 
 plant.B = [0;0; tmp.v3; -tmp.v3];
 
-plant.Bd = [0;0; -tmp.v1; tmp.v1];
+plant.C = [1,0,0,0];
+plant.D = 0;
 
 
 %% Feedback Controller Design
 % Desired dynamic parameters for approximation
-eigP.damping = log(1/specs.mp) / sqrt(pi^2 + log(1/specs.mp)^2); 
+eigP.damping = log(1/specs.mp) / sqrt(pi^2 + log(1/specs.mp)^2);
 eigP.wn = 3/(eigP.damping*specs.settling_time);
 
 % Phase of eigenvalues
 eigP.phi = atan2(sqrt(1-eigP.damping^2), eigP.damping);
 
 % Desired eigenvalues for nominal tracking
-eigP.values = eigP.wn*exp(1i*[-pi+eigP.phi, -pi-eigP.phi, ...
-                              -pi+eigP.phi/2, -pi-eigP.phi/2]);
+eigP.values = eigP.wn*exp(1i*[-pi+eigP.phi,-pi+eigP.phi/2]);
 
 % State feedback matrix
-feedback.K = acker(plant.A, plant.B, eigP.values);
+feedback.K = acker(plant.A, plant.B, [eigP.values, conj(eigP.values)]);
 
 
-%{
+
 % State feedforward gain and input feedforward gain
-feedback.gains = ([plant.A, plant.B; plant.C, plant.D])\[0;0;1];
-feedback.Nx = feedback.gains(1:2);
-feedback.Nu = feedback.gains(3);
-
+feedback.gains = ([plant.A, plant.B; plant.C, plant.D])\[zeros(4,1);1];
+feedback.Nx = feedback.gains(1:end-1);
+feedback.Nu = feedback.gains(end);
+%{
 % Eigenvalues for robust tracking
 eig.robustValues = [eig.real + 1i*eig.img, ...
                      eig.real - 1i*eig.img, ...
