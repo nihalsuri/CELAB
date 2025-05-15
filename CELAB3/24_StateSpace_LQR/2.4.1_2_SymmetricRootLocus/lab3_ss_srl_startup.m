6f92171f0b986ba750d2ba6d42723d785bb550bc
nihalsuri <nihalsuri1619@gmail.com>
2025-05-15 17:04:25 +0200

    added robust srl

--- CELAB3/24_StateSpace_LQR/2.4.1_2_SymmetricRootLocus/lab3_ss_srl_startup.m
+++ CELAB3/24_StateSpace_LQR/2.4.1_2_SymmetricRootLocus/lab3_ss_srl_startup.m
@@ -13,7 +13,6 @@ load_params_model
 
 
 %% User Inputs
-
 % The choice of r should satisfy the following specs mentioned below: 
 % Desired specifications
 % Overshoot
@@ -59,17 +58,13 @@ plant.C = [1,0,0,0];
 plant.D = 0;
 
 
-%% Feedback Controller Design using Symmetric Root Locus
+%% Feedback Controller Design using Symmetric Root Locus (Nominal)
 % % % Desired dynamic parameters for approximation
 eigP.damping = log(1/specs.mp) / sqrt(pi^2 + log(1/specs.mp)^2);
 eigP.wn = 3/(eigP.damping*specs.settling_time);
 
 % Phase of eigenvalues
 eigP.phi = atan2(sqrt(1-eigP.damping^2), eigP.damping);
-
-% Desired eigenvalues for nominal tracking
-eigP.values = eigP.wn*exp(1i*[-pi+eigP.phi,-pi+eigP.phi/2]);
-eigP.values = [eigP.values, conj(eigP.values)];
 
 % LTI objects of the plant for sketching of the locus
 plant.sysG = ss(plant.A,plant.B,plant.C,0);
@@ -98,7 +93,7 @@ plot([0, x_end], [0, y_end_neg], 'g--', 'LineWidth', 2);
 % Add legend and improve plot appearance
 legend('Root Locus','Settling Time Constraint', 'Overshoot Constraint');
 grid on;
-title('Symmetric Root Locus with Design Specifications');
+title('Symmetric Root Locus with Design Specifications (Nominal)');
 
 %value obtained from visually examining the rlocus plot 
 feedback.r = 1/3.53e+03;
@@ -109,6 +104,41 @@ feedback.gains = ([plant.A, plant.B; plant.C, plant.D])\[zeros(4,1);1];
 feedback.Nx = feedback.gains(1:end-1);
 feedback.Nu = feedback.gains(end);
 
+%% Feedback Controller Design using Symmetric Root Locus (Robust)
+% Extended statespace model
+plant.Ae = [0, plant.C; zeros(4,1), plant.A];
+plant.Be = [0;plant.B];
+plant.Ce = [plant.C, 0];
+
+% LTI objects of the plant for sketching of the locus
+plant.robustsysG = ss(plant.Ae,plant.Be,plant.Ce,0);
+plant.robustsysGp = ss(-plant.Ae,-plant.Be,plant.Ce,0);
+
+% Plotting the symmetric root locus of the system
+figure;
+rlocus(plant.robustsysG*plant.robustsysGp);
+hold on;
+
+% Add settling time constraint (vertical line)
+xline(-(3/specs.settling_time), 'k--', 'LineWidth', 2);
+hold on; 
+% Add overshoot constraint (radial lines)
+% Plot the two radial lines (positive and negative imaginary)
+plot([0, x_end], [0, y_end_pos], 'g--', 'LineWidth', 2);
+plot([0, x_end], [0, y_end_neg], 'g--', 'LineWidth', 2);
+
+% Add legend and improve plot appearance
+legend('Root Locus','Settling Time Constraint', 'Overshoot Constraint');
+grid on;
+title('Symmetric Root Locus with Design Specifications (Robust)');
+
+%value obtained from visually examining the rlocus plot 
+feedback.robust_r = 1/5.44e+04;
+feedback.robustKe = lqry(plant.robustsysG, 1, feedback.robust_r);
+feedback.robustKi = feedback.robustKe(1);
+feedback.robustK  = feedback.robustKe(2:end);
+
+
 
 %% Simple Observer
 filt.wc = 2*pi*50;
@@ -116,6 +146,6 @@ filt.del = 1/sqrt(2);
 filt.num = [filt.wc^2, 0];
 filt.den = [1, 2*filt.del*filt.wc, filt.wc^2];
 
-%% Evaluation script to check if specs match
+% %% Evaluation script to check if specs match
 sim('lab3_ss_srl')
-evalLQR
+% evalLQR
