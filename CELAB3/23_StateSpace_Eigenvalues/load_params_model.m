60dee6d609bfa06c9ac4d56b0c2015863b44dd03
6maxwell9 <pries.maximilian@t-online.de>
2025-05-15 16:33:33 +0200

    made a mistake in changing the load_params files

--- CELAB3/23_StateSpace_Eigenvalues/load_params_model.m
+++ CELAB3/23_StateSpace_Eigenvalues/load_params_model.m
@@ -1,11 +1,10 @@
 % Choose to load parameters estimated from the motor or nominal
 
-if sIn.motor_or_nominal == 0
-    % Parameters estimated from motor 1 
-    % Motor Friction (lab0)
-    mld.Beq = 2.5663e-6;    % [Nm/(rad/s)]
-    mld.tausf = 0.013;      % [Nm]
-    
+% Motor Friction (lab0)
+mld.Beq = 2.5663e-6;    % [Nm/(rad/s)]
+mld.tausf = 0.013;      % [Nm]
+
+if sIn.motor_or_nominal == 0   
     % Resonant Load (lab3)
     %mld.Bb = ;
     %mld.k = ;
