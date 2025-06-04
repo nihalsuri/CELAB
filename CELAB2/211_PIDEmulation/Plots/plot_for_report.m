6cbce8b07567d29d66df8d37271c0f91911c1fc4
6maxwell9 <pries.maximilian@t-online.de>
2025-06-04 14:04:16 +0200

    Updated ZOH model and plot for sim
    The model had a mixup for sampling times

--- CELAB2/211_PIDEmulation/Plots/plot_for_report.m
+++ CELAB2/211_PIDEmulation/Plots/plot_for_report.m
@@ -7,8 +7,8 @@ clear p i j
 % The reference should be the last channel, as this is the only one to be ignored.
 % The y-label will be extracted from the first entry.
 
-p.simout = [position1ms position10ms position50ms];  % Replace with your actual Simulink output variables
-p.save_name = "backward";
+p.simout = [position, position_pid];  % Replace with your actual Simulink output variables
+p.save_name = "pid_aw_comparison";
 p.time_start = 0;
 p.time_stop  = 1;
 % =========================================================================
--- CELAB2/211_PIDEmulation/plot_for_report.m
+++ CELAB2/211_PIDEmulation/plot_for_report.m
@@ -10,9 +10,9 @@ clear p i j
 % Supports fixed and variable time (also varying for differnt signals)
 
 
-p.simout = [out.ScopeData];
+p.simout = [out.zoh];
 p.fname = "simulation_results\";
-p.save_name = "2.1.1.5_FE";
+p.save_name = "2.1.1.5_ZOH";
 p.time_start = 0;
 p.time_stop  = 1;
 % =========================================================================
@@ -87,5 +87,5 @@ ylim("padded")
 set(p.f1,'Position',[300 300 800 400])
 
 %%save figure as a .png file and save the corresponding data
-%saveas(p.f1, fullfile(p.fname, p.save_name) + ".png");
+saveas(p.f1, fullfile(p.fname, p.save_name) + ".png");
 %save('Data_and_Plots/'+p.save_name+'.mat', "p.simout")
